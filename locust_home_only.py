fc467e3ec67b31c262ce3a6609dd3e90054eea31
gie913 <gie.hermawan@gmail.com>
2020-11-13 14:18:45 +0700

    change home

--- locust_home_only.py
+++ locust_home_only.py
@@ -18,3 +18,34 @@ class QuickstartUser(HttpUser):
 
 
 
+    @task(1)
+
+    def ehs_page(self):
+
+        self.client.get("/ehs-statistic")
+		
+		
+
+    @task(2)
+
+    def news_page(self):
+
+        self.client.get("/news")
+		
+	
+	
+    @task(1)
+
+    def simpul_detail(self):
+
+        self.client.get("/document-viewer/general-content/simpul/2d32c048-1a90-11eb-bc84-506b8d923f0f")
+		
+		
+	@task(1)
+
+    def rss_detail(self):
+
+        self.client.get("/rss-news/detail/0ed552ac-224f-11eb-aa17-00059a3c7a00")
+
+
+			
