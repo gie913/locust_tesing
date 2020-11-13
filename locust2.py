cd1fcca52df825ec198c65670dd069c2b234c160
gie913 <gie.hermawan@gmail.com>
2020-11-13 13:12:25 +0700

    add index only

---
+++ locust2.py
@@ -0,0 +1,16 @@
+import time
+
+from locust import HttpUser, task, between
+
+
+
+class QuickstartUser(HttpUser):
+
+    wait_time = between(1, 2)
+			
+    def on_start(self):
+
+        self.client.get("/info.php")
+
+
+
